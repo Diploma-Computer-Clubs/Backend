1a1d0f7493a20caf00b142982f33e79f8d54dae7
JFN <eserikova22@gmail.com>
2026-03-16 14:31:49 +0500

    fix: fixing models import bugs

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from src.modules.users.router import router as router_users
 from src.modules.auth.router import router as router_auth
 from src.modules.cities.router import router as cities_router
-
+from src.shared.models.model import *
 
 app = FastAPI()
 
