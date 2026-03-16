90d79b4580046c488bc05540a5127b07ee55f687
JFN <eserikova22@gmail.com>
2026-03-16 14:29:33 +0500

    feat: changed models global import

---
+++ src/shared/models/model.py
@@ -0,0 +1,6 @@
+from src.modules.users.model import User
+from src.modules.cities.model import City
+from src.modules.clubs.model import Club
+from src.modules.zones.model import Zone
+from src.modules.computers.model import Computer
+from src.modules.bookings.model import Booking
